e9aa6125244535a838328320655dfb4e32c00fff
Thibaud Priou <thibaud.priou@gmail.com>
2021-08-09 19:10:15 +0900

    create custom Carousel element
    Warning: slides transitions need to be fixed

---
+++ src/components/Carousel.svelte
@@ -0,0 +1,134 @@
+<script lang="ts">
+  import CarouselControl from "./CarouselControl.svelte";
+  import CarouselIndicator from "./CarouselIndicator.svelte";
+  import type IRealisation from "../types/realisation";
+
+  // --- props
+  export let samples: IRealisation["samples"];
+
+  // ! remove embla-carousel from package.json
+
+  // --- data
+  let activeSlideIdx: number = 0;
+  const TRANSITION_DURATION = 2000;
+
+  // --- reactive
+  $: slides = samples
+    .sort((s1, s2) => s1.rank - s2.rank)
+    .map((s, idx) => {
+      const attrs = getSampleImgAttributes(s);
+      return { ...s, src: attrs.src, srcset: attrs.srcset, idx };
+    });
+
+  $: canGoPrev = activeSlideIdx > 0;
+  $: canGoNext = activeSlideIdx < slides.length - 1;
+
+  // --- methods
+  interface ImgAttributes {
+    src: string;
+    srcset: string;
+  }
+
+  function getSampleImgAttributes(
+    sample: IRealisation["samples"][0]
+  ): ImgAttributes {
+    return {
+      src: sample.defaultImagePath,
+      srcset: sample.images.map((i) => `${i.path} ${i.breakpoint}`).join(","),
+    };
+  }
+
+  function goPrev() {
+    activeSlideIdx = (activeSlideIdx - 1) % slides.length;
+  }
+
+  function goNext() {
+    goTo((activeSlideIdx + 1) % slides.length);
+  }
+
+  function goTo(index: number) {
+    activeSlideIdx = index;
+  }
+</script>
+
+<div class="slides-container">
+  {#each slides as slide}
+    {#if slide.idx === activeSlideIdx}
+      <img
+        class="slide"
+        src={slide.src}
+        srcset={slide.srcset}
+        alt={slide.alt}
+      />
+    {/if}
+  {/each}
+</div>
+{#if canGoPrev}
+  <button class="control prev" on:click={goPrev}><CarouselControl /></button>
+{/if}
+{#if canGoNext}
+  <button class="control next" on:click={goNext}
+    ><CarouselControl invert /></button
+  >
+{/if}
+<span class="indicators">
+  {#each slides as _, idx}
+    <button class="indicator" on:click={() => goTo(idx)}>
+      <CarouselIndicator active={idx === activeSlideIdx} />
+    </button>
+  {/each}
+</span>
+
+<style>
+  .control {
+    position: absolute;
+    top: 0;
+    height: 100%;
+    width: 3em;
+    background: none;
+    border: none;
+    fill: white;
+    z-index: 1;
+    padding: 0 4em;
+    cursor: pointer;
+  }
+
+  .control.prev {
+    left: 0;
+  }
+
+  .control.next {
+    right: 0;
+  }
+
+  .indicators {
+    position: absolute;
+    bottom: 2.5vw;
+    width: 100%;
+    z-index: 1;
+    display: flex;
+    align-content: center;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .indicator {
+    cursor: pointer;
+    margin: 0 3vw;
+    width: 10vw;
+    background: none;
+    border: none;
+    height: 0.45em;
+    position: relative;
+  }
+
+  .indicator::after {
+    content: "";
+    position: absolute;
+    top: calc(50% - 1.5em);
+    left: -1em;
+    width: 10em;
+    height: 3em;
+    z-index: -1;
+  }
+</style>
--- src/components/Realisation.svelte
+++ src/components/Realisation.svelte
@@ -1,68 +1,8 @@
 <script lang="ts">
-  import EmblaCarousel from "embla-carousel";
-  import { onMount } from "svelte";
-
   import type IRealisation from "../types/realisation";
-  import CarouselControl from "./CarouselControl.svelte";
-  import CarouselIndicator from "./CarouselIndicator.svelte";
+  import Carousel from "./Carousel.svelte";
 
   export let realisation: IRealisation;
-
-  $: samples = realisation.samples
-    .sort((s1, s2) => s1.rank - s2.rank)
-    .map((s) => {
-      const attrs = getSampleImgAttributes(s);
-      return { ...s, src: attrs.src, srcset: attrs.srcset };
-    });
-
-  function getSampleImgAttributes(sample: IRealisation["samples"][0]): {
-    src: string;
-    srcset: string;
-  } {
-    return {
-      src: sample.defaultImagePath,
-      srcset: sample.images.map((i) => `${i.path} ${i.breakpoint}`).join(","),
-    };
-  }
-
-  let emblaNode;
-  let embla;
-  let canScrollPrev: boolean = false;
-  let canScrollNext: boolean = false;
-  let activeSampleIdx: number = 0;
-
-  onMount(() => {
-    const options = { loop: false, speed: 8, dragFree: true };
-    embla = EmblaCarousel(emblaNode as HTMLElement, options);
-    embla.on("init", updateControls);
-    embla.on("select", updateControls);
-
-    // lazy-loading of hidden images
-    setTimeout(() => {
-      for (const img of emblaNode.querySelectorAll('.lazy-loaded') as HTMLElement[]) {
-       img.setAttribute('src', img.getAttribute('data-src'))
-       img.setAttribute('srcset', img.getAttribute('data-srcset'))
-      }
-    }, 1000)
-  });
-
-  function scrollPrev() {
-    embla.scrollPrev();
-  }
-
-  function scrollNext() {
-    embla.scrollNext();
-  }
-
-  function updateControls() {
-    activeSampleIdx = embla.selectedScrollSnap();
-    canScrollPrev = activeSampleIdx > 0;
-    canScrollNext = activeSampleIdx < samples.length - 1;
-  }
-
-  function scrollTo(index: number) {
-    embla.scrollTo(index);
-  }
 </script>
 
 <section class="container">
@@ -76,41 +16,7 @@
       target="_blank">Voir</a
     >
   </div>
-  <div class="embla" bind:this={emblaNode}>
-    <div class="embla__container">
-      {#each samples as sample, i}
-        <div class="embla__slide">
-          <!-- hidden carousel images's loading is deferred -->
-          <img
-            class="img"
-            src={i === 0 ? sample.src : ""}
-            srcset={i === 0 ? sample.srcset : ""}
-            data-src={sample.src}
-            data-srcset={sample.srcset}
-            loading="lazy"
-            alt={sample.alt}
-          />
-        </div>
-      {/each}
-    </div>
-  </div>
-  {#if canScrollPrev}
-    <button class="control prev" on:click={scrollPrev}
-      ><CarouselControl /></button
-    >
-  {/if}
-  {#if canScrollNext}
-    <button class="control next" on:click={scrollNext}
-      ><CarouselControl invert /></button
-    >
-  {/if}
-  <span class="indicators">
-    {#each samples as _, i}
-      <button class="indicator" on:click={() => scrollTo(i)}>
-        <CarouselIndicator active={i === activeSampleIdx} />
-      </button>
-    {/each}
-  </span>
+  <Carousel samples={realisation.samples}/>
 </section>
 
 <style>
@@ -168,73 +74,5 @@
   .seeVideoLink:hover {
     transform: scale(1.1);
     box-shadow: 2px 2px 10px rgb(0, 0, 0, 0.5);
-  }
-
-  .embla {
-    overflow: hidden;
-  }
-  .embla__container {
-    display: flex;
-  }
-  .embla__slide {
-    position: relative;
-    flex: 0 0 100%;
-    font-size: 0; /* to remove gap under element caused by line-breaks or whitespaces */
-  }
-
-  .img {
-    max-width: 100%;
-  }
-
-  .control {
-    position: absolute;
-    top: 0;
-    height: 100%;
-    width: 3em;
-    background: none;
-    border: none;
-    fill: white;
-    z-index: 1;
-    padding: 0 4em;
-    cursor: pointer;
-  }
-
-  .control.prev {
-    left: 0;
-  }
-
-  .control.next {
-    right: 0;
-  }
-
-  .indicators {
-    position: absolute;
-    bottom: 2.5vw;
-    width: 100%;
-    z-index: 1;
-    display: flex;
-    align-content: center;
-    align-items: center;
-    justify-content: center;
-  }
-
-  .indicator {
-    cursor: pointer;
-    margin: 0 3vw;
-    width: 10vw;
-    background: none;
-    border: none;
-    height: 0.45em;
-    position: relative;
-  }
-
-  .indicator::after {
-    content: "";
-    position: absolute;
-    top: calc(50% - 1.5em);
-    left: -1em;
-    width: 10em;
-    height: 3em;
-    z-index: -1;
   }
 </style>
