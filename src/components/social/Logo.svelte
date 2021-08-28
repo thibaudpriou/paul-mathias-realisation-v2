d531a5b1ae32f2486ab04a81959290249ee5bf13
Thibaud Priou <thibaud.priou@gmail.com>
2021-08-28 20:52:28 +0900

    set random value for social Logos background's sizing
    as I don't have a clue about the real value and lazy client is lazy...

--- src/components/social/Logo.svelte
+++ src/components/social/Logo.svelte
@@ -48,11 +48,13 @@
   
     .social-logo-bg {
       position: absolute;
-      top: -75%;
-      left: -75%;
-      width: 250%;
-      height: 250%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
       z-index: -1;
+      transform: scale(2.5);
     }
   </style>
   
